b3c28e535564dfa1d0803f553c1eaf7a5d1f72ff
rakeshneopane <rakeshneopane@gmail.com>
2025-10-06 13:13:18 +0530

    chore: Typo fixed

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -6,7 +6,7 @@ const mongoUri = process.env.MONGODB;
 
 const initialiseDatabase = async()=>{
     await mongoose.connect(mongoUri).then(()=>{
-        console.log("Connected to tthe Database.");        
+        console.log("Connected to the Database.");        
     }).catch((error)=>{
         console.log("An error occured while connecting to the database.", error);        
     })
